20e5775dad39b85892b6a252591db5359e797227
sakib <nsakib.cse@gmail.com>
2021-07-10 00:00:12 +0600

    Create type alias.

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -5,6 +5,8 @@ export type MemberType = {
   id: string;
 };
 
-export function Member() {
+type MemberProps = { member: MemberType };
+
+export function Member({ member }: MemberProps) {
   return <div data-testid="member">Member</div>;
 }
