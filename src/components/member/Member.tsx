55cf293ce95eed2e13891e58d32a8edca62ac35b
sakib <nsakib.cse@gmail.com>
2021-07-11 01:55:35 +0600

    Add delete button to Member component.

--- src/components/member/Member.tsx
+++ src/components/member/Member.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { store } from "./membersSlice";
 
 export type MemberType = {
   name: string;
@@ -15,6 +16,13 @@ export function Member({ member }: MemberProps) {
       <h1>{member.name}</h1>
       <h1>{member.born}</h1>
       <p>{member.bio}</p>
+      <button
+        onClick={() =>
+          store.dispatch({ type: "DELETE_MEMBER", payload: member.id })
+        }
+      >
+        DELETE
+      </button>
     </div>
   );
 }
