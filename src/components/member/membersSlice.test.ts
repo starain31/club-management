d435569a063fb78e66d20b1257416b02cf60bab9
sakib <nsakib.cse@gmail.com>
2021-07-10 17:44:14 +0600

    Implement UPDATE_MEMBER action.

--- src/components/member/membersSlice.test.ts
+++ src/components/member/membersSlice.test.ts
@@ -80,3 +80,87 @@ describe("DELETE_MEMBER", () => {
     ).toEqual([]);
   });
 });
+
+describe("UPDATE_MEMBER", () => {
+  test("Update a single key", () => {
+    const state_before_update_member = [
+      {
+        name: "Edsger W. Dijkstra",
+        born: "May 11, 1940",
+        bio: "Edsger Wybe Dijkstra was a Dutch computer scientist.",
+        id: "2",
+      },
+      {
+        name: "Alan Kay",
+        born: "May 17, 1940",
+        bio: "Alan Curtis Kay is an American computer scientist.",
+        id: "1",
+      },
+    ];
+
+    const state_after_update_member = [
+      {
+        name: "Edsger W. Dijkstra",
+        born: "May 11, 1930",
+        bio: "Edsger Wybe Dijkstra was a Dutch computer scientist.",
+        id: "2",
+      },
+      {
+        name: "Alan Kay",
+        born: "May 17, 1940",
+        bio: "Alan Curtis Kay is an American computer scientist.",
+        id: "1",
+      },
+    ];
+
+    expect(
+      members(state_before_update_member, {
+        type: "UPDATE_MEMBER",
+        payload: { id: "2", born: "May 11, 1930" },
+      })
+    ).toEqual(state_after_update_member);
+  });
+
+  test("update multiple key", () => {
+    const state_before_update_member = [
+      {
+        name: "Edsger W. Dijkstra",
+        born: "May 11, 1940",
+        bio: "Edsger Wybe Dijkstra was a Dutch computer scientist.",
+        id: "2",
+      },
+      {
+        name: "Alan Kay",
+        born: "May 17, 1940",
+        bio: "Alan Curtis Kay is an American computer scientist.",
+        id: "1",
+      },
+    ];
+
+    const state_after_update_member = [
+      {
+        name: "Edsger W. Dijkstra",
+        born: "May 11, 1930",
+        bio: "Edsger Wybe Dijkstra was a Dutch computer scientist, programmer",
+        id: "2",
+      },
+      {
+        name: "Alan Kay",
+        born: "May 17, 1940",
+        bio: "Alan Curtis Kay is an American computer scientist.",
+        id: "1",
+      },
+    ];
+
+    expect(
+      members(state_before_update_member, {
+        type: "UPDATE_MEMBER",
+        payload: {
+          id: "2",
+          born: "May 11, 1930",
+          bio: "Edsger Wybe Dijkstra was a Dutch computer scientist, programmer",
+        },
+      })
+    ).toEqual(state_after_update_member);
+  });
+});
--- src/components/member/membersSlice.ts
+++ src/components/member/membersSlice.ts
@@ -1,29 +1,41 @@
-import {MemberType} from "./Member";
+import { MemberType } from "./Member";
 
 type MembersState = MemberType[];
-type ActionType = AddMemberAction | DeleteMemberAction;
+type ActionType = AddMemberAction | DeleteMemberAction | UpdateMemberAction;
 
 type AddMemberAction = {
-    type: 'ADD_MEMBER';
-    payload: MemberType;
-}
+  type: "ADD_MEMBER";
+  payload: MemberType;
+};
 
 type DeleteMemberAction = {
-    type: 'DELETE_MEMBER';
-    payload: string;
-}
+  type: "DELETE_MEMBER";
+  payload: string;
+};
+
+type UpdateMemberAction = {
+  type: "UPDATE_MEMBER";
+  payload: {
+    id: string;
+    name?: string;
+    born?: string;
+    bio?: string;
+  };
+};
 
 export const members = (state: MembersState = [], action: ActionType) => {
-    switch (action.type) {
-      case "ADD_MEMBER":
-          return [
-              ...state,
-              action.payload
-          ]
-      case "DELETE_MEMBER":
-          return state.filter((member) => member.id !== action.payload);
-        default:
-            return state;
-    }
-
-}
+  switch (action.type) {
+    case "ADD_MEMBER":
+      return [...state, action.payload];
+    case "DELETE_MEMBER":
+      return state.filter((member) => member.id !== action.payload);
+    case "UPDATE_MEMBER":
+      return state.map((member) => {
+        return member.id === action.payload.id
+          ? { ...member, ...action.payload }
+          : member;
+      });
+    default:
+      return state;
+  }
+};
